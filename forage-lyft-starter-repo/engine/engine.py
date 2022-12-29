832fbf1ab6f5061414e50a29c12434fff97b3ec3
what-a-nice-name <56292774+what-a-nice-name@users.noreply.github.com>
2022-12-29 16:45:18 +0530

    Delete forage-lyft-starter-repo directory

--- forage-lyft-starter-repo/engine/engine.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC
-
-
-Class Engine(ABC):
-    def needs_service(self):
-    pass
--- forage-lyft-starter-repo/service.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class Servicebale(ABC):
-    @abstractmethod
-    def needs_service(self):
-        pass
